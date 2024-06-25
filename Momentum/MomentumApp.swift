cf1ad8c56ea91523eb86291eedd2d59a6fa1396c
Alder Whiteford <alderjwhiteford@gmail.com>
2024-06-25 09:02:29 -0400

    Initial Commit

---
+++ Momentum/MomentumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentumApp.swift
+//  Momentum
+//
+//  Created by Alder Whiteford on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MomentumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
