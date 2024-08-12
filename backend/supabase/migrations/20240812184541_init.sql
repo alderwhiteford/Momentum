1eb07f574317ed37ba03dc25d0c2e46cdbec970b
Alder Whiteford <alderjwhiteford@gmail.com>
2024-08-12 18:44:06 -0400

    remove comma

--- backend/supabase/migrations/20240812184541_init.sql
+++ backend/supabase/migrations/20240812184541_init.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS users (
     user_id uuid PRIMARY KEY,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     first_name text NOT NULL,
-    last_name text NOT NULL,
+    last_name text NOT NULL
 );
