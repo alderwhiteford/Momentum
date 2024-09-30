7b217a9c32a352c6a43e56f32a34221cd8583995
Alder Whiteford <alderjwhiteford@gmail.com>
2024-09-30 18:27:19 -0400

    hopefully fix repo issues

--- ios/MomentumIOS/MomentumIOS/Errors/ErrorManager.swift
+++ ios/MomentumIOS/MomentumIOS/Errors/ErrorManager.swift
@@ -2,25 +2,7 @@
 //  ErrorManager.swift
 //  MomentumIOS
 //
-//  Created by Alder Whiteford on 9/28/24.
+//  Created by Alder Whiteford on 9/30/24.
 //
 
 import Foundation
-
-class ErrorManager: ObservableObject {
-    @Published var errorMessage: String
-    
-    init() {
-        self.errorMessage = ""
-    }
-    
-    func setError(errorMessage: String) -> Void {
-        // Update the error message:
-        self.errorMessage = errorMessage
-        
-        // Clear the error after 4 seconds:
-        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
-            self.errorMessage = ""
-        }
-    }
-}
